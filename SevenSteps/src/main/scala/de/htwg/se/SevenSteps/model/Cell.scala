94c124acf93716720d60dff6f8c75f8a2759d953
tobias <tobias.grimm@gmx.net>
2017-04-05 14:12:02 +0200

    toString for Cell

--- SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Cell.scala
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Cell.scala
@@ -1,5 +1,7 @@
 package de.htwg.se.SevenSteps.model
 
 case class Cell(color: String,height: Int) {
+  def this(){this(" ",0)}
+  override def toString = {if (color!=" "){color.charAt(0).toString()+" "+height}else{"   "}}
 }
 
--- SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Grid.scala
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Grid.scala
@@ -2,7 +2,8 @@ package de.htwg.se.SevenSteps.model
 
 case class Grid(rows: Int,cols: Int) {
   
-  val grid=Array.ofDim[Cell](rows, cols)
+  val grid=Array.fill(rows, cols)(new Cell)
+  
   
   override def toString = {
     val linesep = "+---"*cols+"+\n"
@@ -10,7 +11,7 @@ case class Grid(rows: Int,cols: Int) {
     var strGrid = "\n"+(linesep+line)*rows+linesep
     for (row <- 0 until rows;
          col <- 0 until cols){
-      strGrid = strGrid.replaceFirst("XXX", "   ")
+      strGrid = strGrid.replaceFirst("XXX", grid(row)(col).toString)
     }
     strGrid
   }
