238073a8287054b06d77bc6539ca549da852b006
Julius Kirchdörfer <mumm_1@yahoo.de>
2017-04-06 15:15:02 +0200

    new class Player

---
+++ SevenSteps/src/test/scala/de/htwg/se/SevenSteps/model/PlayerSpec.scala
@@ -0,0 +1,23 @@
+package de.htwg.se.SevenSteps.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.Matchers._
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class PlayerSpec extends WordSpec {
+  
+  "A Player" should {
+    
+    "have a name" in {
+      Player("Julius", 50).name should be("Julius")
+    }
+    "have points" in {
+      Player ("Julius", 50).points should be (50)
+    }
+  }
+  
+  
+}
