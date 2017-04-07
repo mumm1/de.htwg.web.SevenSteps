d4f9b9888c4251fd10ca31fea589432e8d161f05
Julius Kirchdörfer <mumm_1@yahoo.de>
2017-04-07 09:28:58 +0200

    edit player test

--- SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Player.scala
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Player.scala
@@ -2,27 +2,24 @@ package de.htwg.se.SevenSteps.model
 
 case class Player (name : String, points : Int) {
 
-  var colors =Array {'a';'b';'c'}
-  
-	val map = collection.mutable.Map[Char, Int]()
+	private val colors =Array ('a','b','c','g')
 
-  for ((c) <- colors){
-    map += (c -> 0)
-    
-  }
-  
-  
+	 val map = collection.mutable.ListMap[Char, Int]()
+
+			for ((c) <- colors){  
+				map += (c -> 0)
+			}
 
 
 
-			def add_stone (color: String, number : Int){ map(color) = map.getOrElse(color, 999) + number}
-	def add_stone(color: String )
+
+//	def add_stone (color: Char, number : Int){ map(color) = map.getOrElse(color, 999) + number}
+	//	def add_stone(color: Char ,place : Int ){ }
 
 
 	override def toString = {
 			val sb = new StringBuilder
 					for ((k,v) <- map){ sb.append(k + " --> " + v + "\n")}
-			sb.append(map get "green")
 			sb.toString()
 
 	}
