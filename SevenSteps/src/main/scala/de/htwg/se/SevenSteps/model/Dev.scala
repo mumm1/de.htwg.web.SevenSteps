da57ed9db294a55a59963fa78551d80b7883b506
tobias <tobias.grimm@gmx.net>
2017-04-08 12:39:57 +0200

    Dev file and updates in gitignore

---
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/model/Dev.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.SevenSteps.model
+
+object Dev {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World")
+    val grid = Grid(2,5)
+    println(new Grid(colors="aab  sass",cols=4))
+  }
+}
