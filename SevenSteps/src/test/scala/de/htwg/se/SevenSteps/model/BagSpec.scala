1d70775e1c43a107a093196d36db598eefc6c880
Julius Kirchdörfer <mumm_1@yahoo.de>
2017-04-12 14:45:16 +0200

    Update to master

---
+++ SevenSteps/src/test/scala/de/htwg/se/SevenSteps/model/BagSpec.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.SevenSteps.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.Matchers._
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class BagSpec extends WordSpec  {
+  
+}
