a13fc239b530124fa3db76c6c260bd01f1765923
tobias <tobias.grimm@gmx.net>
2017-04-13 16:50:52 +0200

    undo and redo now returns "Try" (success or failure)

--- SevenSteps/src/main/scala/de/htwg/se/SevenSteps/controller/Controller.scala
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/controller/Controller.scala
@@ -19,25 +19,28 @@ case class Controller(var grid:Grid=new Grid(0,0),var players:List[Player]=Nil) 
     case Failure(e) => message=e.getMessage}
     explored;}
   
-  def undo(){
+  def undo():Try[String]={
     if (undoStack.length>0){
-      val temp = undoStack.pop() 
-      message = "Undo: "+temp.undo(this)
-      redoStack.push(temp)
+      val temp = undoStack.pop() ; val temp2=temp.undo(this)
+       temp2 match {
+        case Success(s) => message="Undo: "+s;redoStack.push(temp)
+        case Failure(e) => message=e.getMessage()}
+      temp2
     }
-    else
-      message="Can't undo now!"
+    else{
+      message="Can't undo now!"; Failure(new Exception(message))}
     }
-  def redo(){
+  
+  def redo():Try[String]={
     if (redoStack.length>0){
-      val temp=redoStack.pop() 
-      temp.doIt(this) match {
-        case Success(s) => message="Redo: "+s
-        case Failure(e) => message=e.getMessage()
-      }
-      undoStack.push(temp)}
-    else
-      message="Can't redo now!"
+      val temp=redoStack.pop() ; val temp2=temp.doIt(this)
+      temp2 match {
+        case Success(s) => message="Redo: "+s;undoStack.push(temp)
+        case Failure(e) => message=e.getMessage()}
+      temp2
+    }
+    else{
+      message="Can't redo now!";Failure(new Exception(message))}
     }
   
   override def toString = {
@@ -57,18 +60,18 @@ case class Controller(var grid:Grid=new Grid(0,0),var players:List[Player]=Nil) 
 // ##################### Controller Commands ####################### 
 
 trait Command {def doIt(c:Controller):Try[String] 
-               def undo(c:Controller):String}
+               def undo(c:Controller):Try[String]}
 
 case class AddPlayer(name:String) extends Command{
   val player= Player(name)
   override def doIt(c:Controller):Try[String]={c.players=c.players:+player;Success("Added Player "+name)}
-  override def undo(c:Controller):String={val temp=c.players.last;c.players=c.players.take(c.players.length - 1);"Deleted Player "+temp.name}
+  override def undo(c:Controller):Try[String]={val temp=c.players.last;c.players=c.players.take(c.players.length - 1);Success("Deleted Player "+temp.name)}
 }
 
 case class NewGrid(colors:String,cols:Int) extends Command{
   var oldGrid:Grid = null
   override def doIt(c:Controller):Try[String]={oldGrid=c.grid;c.grid=new Grid(colors,cols);Success("Build new Grid")}
-  override def undo(c:Controller):String={c.grid=oldGrid;"Deleted new Grid"}
+  override def undo(c:Controller):Try[String]={c.grid=oldGrid;Success("Deleted new Grid")}
 }
 
 case class StartGame() extends Command{
@@ -76,12 +79,12 @@ case class StartGame() extends Command{
     if (c.players.length>0){
       c.gameState=new Play(c);c.undoStack.clear();Success("Started the game")
     }else{Failure(new Exception("Can't start the game: Not enough Players"))}}
-  override def undo(c:Controller):String={c.undoStack.clear();"You can't undo the start of the game!"}
+  override def undo(c:Controller):Try[String]={c.undoStack.clear();Failure(new Exception("You can't undo the start of the game!"))}
 }
 
 case class NextPlayer() extends Command{
   override def doIt(c:Controller):Try[String]={c.curPlayer+=1;c.curPlayer=c.curPlayer%c.players.length;Success("Player "+c.getCurPlayer.name+" it is your turn!")}
-  override def undo(c:Controller):String={c.undoStack.clear();"You can't undo to previous player!"}
+  override def undo(c:Controller):Try[String]={c.undoStack.clear();Failure(new Exception("You can't undo to previous player!"))}
 }
 
 
--- SevenSteps/src/test/scala/de/htwg/se/SevenSteps/controller/ControllerSpec.scala
+++ SevenSteps/src/test/scala/de/htwg/se/SevenSteps/controller/ControllerSpec.scala
@@ -16,14 +16,14 @@ class ControllerSpec extends WordSpec{
       c.exploreCommand(new AddPlayer("Hugo")).isSuccess should be(true)
       c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
       c.players.length should be(2)
-      c.undo()
+      c.undo().isSuccess should be(true)
       c.players.length should be(1)
     }
     "generate a new Grid and undo this" in {
       var c = Controller()
       c.exploreCommand(new NewGrid("ab sdd",3)).isSuccess should be(true)
       c.grid.cellsToString() should be("ab sdd")
-      c.undo()
+      c.undo().isSuccess should be(true)
       c.grid.cellsToString() should be("")
     }
     "start the game (minimum 1 Player) and can't undo that" in {
@@ -34,7 +34,7 @@ class ControllerSpec extends WordSpec{
       c.exploreCommand(new AddPlayer("Hugo"))
       c.exploreCommand(new StartGame()).isSuccess should be(true)
       c.gameState.isInstanceOf[Play] should be(true)      
-      c.undo()
+      c.undo().isSuccess should be(false)
       c.gameState.isInstanceOf[Play] should be(true)
     }
     "do undo redo stuff" in {
@@ -43,25 +43,52 @@ class ControllerSpec extends WordSpec{
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
       c.players.length should be(2)      
-      c.undo()
-      c.undo()
+      c.undo().isSuccess should be(true)
+      c.undo().isSuccess should be(true)
       c.players.length should be(0)
-      c.undo()
+      c.undo().isSuccess should be(false)
       c.players.length should be(0)
-      c.redo()
-      c.redo()
+      c.redo().isSuccess should be(true)
+      c.redo().isSuccess should be(true)
       c.players.length should be(2)  
-      c.redo()
+      c.redo().isSuccess should be(false)
     }
     "generate a cool String" in {
       var c = Controller()
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
+      c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
       c.toString()
     }
+    "can only use prepare commands" in {
+      var c = Controller()
+      c.exploreCommand(new NextPlayer()).isSuccess should be(false)      
+    }
   }
-//  "A Controller in game phase play" should{
-//    ""
-//  }
+  "A Controller in game phase play" should{
+    "switch between different Players and can't undo that" in {
+      var c = Controller() 
+      c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
+      c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
+      c.exploreCommand(new AddPlayer("Alex")).isSuccess should be(true)
+      c.exploreCommand(new StartGame()).isSuccess should be(true)
+      c.getCurPlayer().name should be("Hans")
+      c.exploreCommand(new NextPlayer()).isSuccess should be(true)   
+      c.getCurPlayer().name should be("Peter")      
+      c.exploreCommand(new NextPlayer()).isSuccess should be(true)   
+      c.getCurPlayer().name should be("Alex")  
+      c.exploreCommand(new NextPlayer()).isSuccess should be(true)   
+      c.getCurPlayer().name should be("Hans") 
+      c.undo().isSuccess should be(false)
+      
+    }
+    "can only use play commands" in {
+      var c = Controller()
+      c.gameState=Play(c)
+      c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(false)   
+      c.exploreCommand(new NewGrid("ab sdd",3)).isSuccess should be(false) 
+      c.exploreCommand(new StartGame()).isSuccess should be(false)
+    }
+  }
   
   
 }
