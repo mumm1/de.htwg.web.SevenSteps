5ac5154ad840d7ea508863d93682724008b4843e
tobias <tobias.grimm@gmx.net>
2017-04-15 13:44:53 +0200

    before @ ControllerSpec

--- SevenSteps/src/test/scala/de/htwg/se/SevenSteps/controller/ControllerSpec.scala
+++ SevenSteps/src/test/scala/de/htwg/se/SevenSteps/controller/ControllerSpec.scala
@@ -10,9 +10,21 @@ import de.htwg.se.SevenSteps.model._
 @RunWith(classOf[JUnitRunner])
 class ControllerSpec extends WordSpec{
   
+  var c = Controller()
+  def before(state:GameState) {
+    c = Controller()
+    state match {
+      case p:Prepare => 
+      case p:Play    => { c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
+                          c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
+                          c.exploreCommand(new AddPlayer("Alex")).isSuccess should be(true)
+                          c.exploreCommand(new NewGrid("a",1)).isSuccess should be(true)
+                          c.exploreCommand(new StartGame()).isSuccess should be(true)   }
+    }}
+
   "A Controller in game phase prepare" should{   
     "add Players and undo this" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.exploreCommand(new AddPlayer("Hugo")).isSuccess should be(true)
       c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
       c.players.length should be(2)
@@ -20,14 +32,14 @@ class ControllerSpec extends WordSpec{
       c.players.length should be(1)
     }
     "generate a new Grid and undo this" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.exploreCommand(new NewGrid("ab sdd",3)).isSuccess should be(true)
       c.grid.cellsToString() should be("ab sdd")
       c.undo().isSuccess should be(true)
       c.grid.cellsToString() should be("")
     }
     "start the game (minimum 1 Player) and can't undo that" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.gameState.isInstanceOf[Prepare] should be(true)
       c.exploreCommand(new StartGame()).isSuccess should be(false)
       c.gameState.isInstanceOf[Prepare] should be(true)
@@ -38,7 +50,7 @@ class ControllerSpec extends WordSpec{
       c.gameState.isInstanceOf[Play] should be(true)
     }
     "do undo redo stuff" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.players.length should be(0)  
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
@@ -54,24 +66,23 @@ class ControllerSpec extends WordSpec{
       c.redo().isSuccess should be(false)
     }
     "generate a cool String" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
       c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
       c.toString()
     }
     "can only use prepare commands" in {
-      var c = Controller()
+      before(new Prepare(c))
       c.exploreCommand(new NextPlayer()).isSuccess should be(false)
       c.exploreCommand(new SetStonde(0,0)).isSuccess should be(false)
     }
   }
+  
+  
+  
   "A Controller in game phase play" should{
     "switch between different Players and can't undo that" in {
-      var c = Controller() 
-      c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
-      c.exploreCommand(new AddPlayer("Peter")).isSuccess should be(true)
-      c.exploreCommand(new AddPlayer("Alex")).isSuccess should be(true)
-      c.exploreCommand(new StartGame()).isSuccess should be(true)
+      before(new Play(c))
       c.getCurPlayer().name should be("Hans")
       c.exploreCommand(new NextPlayer()).isSuccess should be(true)   
       c.getCurPlayer().name should be("Peter")      
@@ -83,26 +94,20 @@ class ControllerSpec extends WordSpec{
       
     }
     "can only use play commands" in {
-      var c = Controller()
-      c.gameState=Play(c)
+      before(new Play(c))
       c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(false)   
       c.exploreCommand(new NewGrid("ab sdd",3)).isSuccess should be(false) 
       c.exploreCommand(new StartGame()).isSuccess should be(false)
     }
     "allow a player to set a stone on the grid" in{
-      var c = Controller() 
-      c.exploreCommand(new AddPlayer("Hans")).isSuccess should be(true)
-      c.exploreCommand(new NewGrid("a",1)).isSuccess should be(true)
-      c.exploreCommand(new StartGame()).isSuccess should be(true)
-      
+      before(new Play(c))
       c.exploreCommand(new SetStonde( 0, 0)).isSuccess should be(true)
       c.grid.cell(0,0).height should be(1)
       c.exploreCommand(new SetStonde( 1, 0)).isSuccess should be(false)
       c.exploreCommand(new SetStonde( 0, 1)).isSuccess should be(false)
       c.exploreCommand(new SetStonde(-1, 0)).isSuccess should be(false)
       c.exploreCommand(new SetStonde( 0,-1)).isSuccess should be(false)
-      c.grid.cell(0,0).height should be(1)      
-      
+      c.grid.cell(0,0).height should be(1)           
     }
   }
   
