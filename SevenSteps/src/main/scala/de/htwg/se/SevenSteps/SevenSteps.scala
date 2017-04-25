32990c18518490bf5d541c88c49518c911bed0b5
Tobias <tobias.grimm1408@googlemail.com>
2017-04-25 20:43:05 +0200

    Style Guide stuff

--- SevenSteps/src/main/scala/de/htwg/se/SevenSteps/SevenSteps.scala
+++ SevenSteps/src/main/scala/de/htwg/se/SevenSteps/SevenSteps.scala
@@ -2,11 +2,12 @@ package de.htwg.se.SevenSteps
 
 import de.htwg.se.SevenSteps.controller._
 import de.htwg.se.SevennSteps.aview.tui._
+
 import scala.io.StdIn.readLine
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    val con = new Controller()
+    val con = Controller()
     val tui = new Tui(con)
     while (tui.processInputLine(readLine())) {}
   }
