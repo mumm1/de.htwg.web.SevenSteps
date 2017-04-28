367eedb2e3fa8e2360fe11bf535c44e954928e83
Tobias <tobias.grimm1408@googlemail.com>
2017-04-28 11:53:50 +0200

    new structure of code

---
+++ src/main/scala/de/htwg/se/SevenSteps/model/Player.scala
@@ -0,0 +1,88 @@
+package de.htwg.se.SevenSteps.model
+
+import scala.collection.immutable.Map
+
+case class Players(curPlayer: Int = 0, players: Vector[Player] = Vector()) {
+  def push(player: Player): Players = {
+    copy(players = players :+ player)
+  }
+  def pop(): Players = {
+    copy(players = players.init)
+  }
+  def length: Int = {
+    players.length
+  }
+  def updateCurPlayer(player: Player): Players = {
+    copy(players = players.updated(curPlayer, player))
+  }
+  def next(): Players = {
+    copy(curPlayer = (curPlayer + 1) % players.length)
+  }
+  def nonEmpty: Boolean = {
+    players.nonEmpty
+  }
+  def getCurPlayer: Player = {
+    players(curPlayer)
+  }
+  def setColors(colors: List[Char]): Players = {
+    val newPlayers = for (p <- players) yield {
+      p.setColors(colors)
+    }
+    copy(players = newPlayers)
+  }
+  def setAllStonesTo(num: Int): Players = {
+    val newPlayers = for (p <- players) yield {
+      p.setStones(num)
+    }
+    copy(players = newPlayers)
+  }
+  def apply(i: Int): Player = players(i)
+  override def toString: String = {
+    var text = ""
+    for (player <- players) {
+      if (player == players(curPlayer)) {
+        text += "-> " + player.toString() + "\n"
+      } else {
+        text += "   " + player.toString() + "\n"
+      }
+    }
+    text
+  }
+}
+
+case class Player(name: String, points: Int = 0, map: Map[Char, Int] = Map[Char, Int]()) {
+  def setColors(colors: List[Char]): Player = {
+    var newMap: Map[Char, Int] = Map()
+    for ((c) <- colors) {
+      newMap = newMap + (c -> 0)
+    }
+    copy(map = newMap)
+  }
+  def setStones(num: Int): Player = {
+    var newMap: Map[Char, Int] = Map()
+    for ((k, _) <- map) {
+      newMap = newMap + (k -> num)
+    }
+    copy(map = newMap)
+  }
+  def incPoints(delta: Int): Player = {
+    copy(points = points + delta)
+  }
+  def incColor(color: Char, delta: Int): Player = {
+    copy(map = map.updated(color, map(color) + delta))
+  }
+  def getStoneNumber: Int = {
+    var num = 0
+    for ((_, v) <- map) num += v
+    num
+  }
+  override def toString: String = {
+    val sb = new StringBuilder
+    sb.append(name + ": ")
+    for ((k, v) <- map) {
+      sb.append(k + "=" + v + ", ")
+    }
+    sb.append("Points=" + points)
+    sb.toString()
+  }
+}
