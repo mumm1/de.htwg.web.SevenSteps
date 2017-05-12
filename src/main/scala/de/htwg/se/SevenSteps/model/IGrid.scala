462f355d1f40a990cdfdf2acdd9b918c17b84284
tobias <tobias.grimm@gmx.net>
2017-05-12 12:46:30 +0200

    Added Interfaces

--- src/main/scala/de/htwg/se/SevenSteps/model/IGrid.scala
+++ src/main/scala/de/htwg/se/SevenSteps/model/IGrid.scala
@@ -1,12 +1,11 @@
 package de.htwg.se.SevenSteps.model
 
-import de.htwg.se.SevenSteps.model.impl.Cell
 import scala.util.Try
 
 trait IGrid {
   def set(row: Int, col: Int, color: Char): IGrid
   def set(row: Int, col: Int, height: Int): IGrid
-  def cell(row: Int, col: Int): Try[Cell]
+  def cell(row: Int, col: Int): Try[ICell]
   def nonEmpty: Boolean
   def getColors: List[Char]
   def getHeights: List[Int]
