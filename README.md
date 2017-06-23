e4a3963e6e1dbdef7c9c4b5fe3083c6dddf9285b
tobias <tobias.grimm@gmx.net>
2017-06-23 09:46:25 +0200

    - Test sharing for xml and json - Markdown README updated

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ class Software Engineering at the University of Applied Science HTWG Konstanz
 * Test Driven Development
 * clean code
 * Dependency Injection
+* Build Tool & Continuous Integration (sbt & Travis CI)
+* Serialization (json, xml)
 * Scala (FP and OOP)
 
 
@@ -27,4 +29,41 @@ class Software Engineering at the University of Applied Science HTWG Konstanz
 * Command Pattern (for undo-redo Stack)
 * State Pattern (for game phases)
 * Factory Pattern (alternative to Dependency Injection)
+
+### Other cool stuff
+
+* Scala Mixins for [sharing tests][1] between different implementations of the FileIO Component
+* Automated json mapping with the [Genson][2]-Framework
+* Code Coverage visible in github through [Codecov][3]
+
+### Game Overview
+
+##### Prepare the Game
+
+* Add Players
+* Set a grid size you want to play on
+* Color the grid like you want
+
+##### Play the Game
+
+* Every Player draws up to 7 Stones at the beginning of his turn.
+* You can set on the colored grid a stone if the color matches.
+* The first Stone must be set on height 0 on the grid.
+* The next Stone must be set neighboring the last stone and on the same height (or +1) 
+* You can't set on the same grid cell twice in the same turn.
+* You get Points equal to the height you set the stone, so building higher is better.
+
+##### End of Game
+
+* The Game ends when all players can't set on height 0 anymore.
+* The Player with the most points wins the game.
+
+### GUI & Tui
+
+(Update Image)
+[![Foo](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)
+
+[1]: http://www.scalatest.org/user_guide/sharing_tests
+[2]: https://owlike.github.io/genson/
+[3]: https://codecov.io/
 
--- src/test/scala/de/htwg/se/SevenSteps/model/fileIO/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/SevenSteps/model/fileIO/FileIOSpec.scala
@@ -2,6 +2,7 @@ package de.htwg.se.SevenSteps.model.fileIO
 
 import com.google.inject.Guice
 import de.htwg.se.SevenSteps.SevenStepsModule
+import de.htwg.se.SevenSteps.controller.IController
 import de.htwg.se.SevenSteps.controller.basicImpl.{Controller, ControllerState}
 import de.htwg.se.SevenSteps.model.grid.IGridFactory
 import org.junit.runner.RunWith
@@ -10,23 +11,15 @@ import org.scalatest.Matchers.{be, _}
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
-class FileIOSpec extends WordSpec {
-  def getController: Controller = {
-    val injector = Guice.createInjector(new SevenStepsModule)
-    new Controller(injector.getInstance(classOf[ControllerState]),
-      injector.getInstance(classOf[IGridFactory]))
-  }
-  "A FileIO" should {
-    val fileIO = json.FileIO()
-    "can save & restore the default ControllerState" ignore {
-      val c = getController
+trait IOBehaviors {
+  this: WordSpec =>
+  def saveAndLoad(fileIO: IFileIO, c: IController): Unit = {
+    "can save & restore the default ControllerState" in {
       fileIO.save(c.state)
       fileIO.load should be(c.state)
     }
-    "can save & restore a complex ControllerState" ignore {
+    "can save & restore a complex ControllerState" in {
       implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.01)
-      val c = getController
       c
         .addPlayer("Hans").get.addPlayer("Alex").get
         .newGrid("aabb", 2).get.startGame().get
@@ -37,5 +30,20 @@ class FileIOSpec extends WordSpec {
       c2.grid should be(c.state.grid)
       fileIO.load should be(c.state)
     }
+  }
+}
+
+@RunWith(classOf[JUnitRunner])
+class FileIOSpec extends WordSpec with IOBehaviors {
+  def getController: Controller = {
+    val injector = Guice.createInjector(new SevenStepsModule)
+    new Controller(injector.getInstance(classOf[ControllerState]),
+      injector.getInstance(classOf[IGridFactory]))
+  }
+  "A json FileIO" should {
+    behave like saveAndLoad(json.FileIO(), getController)
+  }
+  "A xml  FileIO" should {
+    behave like saveAndLoad(xml.FileIO(), getController)
   }
 }
